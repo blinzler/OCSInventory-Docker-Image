bd1f71953e850101be123d4fefc4cc30980d128a
Martin <brandt.mb@gmail.com>
2018-01-05 15:19:29 +0100

    based on centos7 container

--- 2.4/scripts/run.sh
+++ 2.4/scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ ! -d "$APACHE_RUN_DIR" ]; then
-	mkdir "$APACHE_RUN_DIR"
-	chown $APACHE_RUN_USER:$APACHE_RUN_GROUP "$APACHE_RUN_DIR"
+if [ ! -d "$HTTPD_RUN_DIR" ]; then
+	mkdir "$HTTPD_RUN_DIR"
+	chown $HTTPD_RUN_USER:$HTTPD_RUN_GROUP "$HTTPD_RUN_DIR"
 fi
-if [ -f "$APACHE_PID_FILE" ]; then
-	rm "$APACHE_PID_FILE"
+if [ -f "$HTTPD_PID_FILE" ]; then
+	rm "$HTTPD_PID_FILE"
 fi
 /usr/sbin/apache2ctl -D FOREGROUND
